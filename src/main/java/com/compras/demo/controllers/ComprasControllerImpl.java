84d59146d02868967f887c453d7d3d671ec4f86d
Omar Villanueva <ovillanueva@inspiratls.com>
2021-08-27 00:07:07 -0500

    se agrego cross origin

--- src/main/java/com/compras/demo/controllers/ComprasControllerImpl.java
+++ src/main/java/com/compras/demo/controllers/ComprasControllerImpl.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.compras.demo.beans.Respuesta;
@@ -15,6 +17,7 @@ import com.compras.demo.beans.response.CompraDTOResponse;
 import com.compras.demo.services.ComprasService;
 
 @RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 public class ComprasControllerImpl implements ComprasController {
 
 	@Autowired
